edf7d59e2f2744e73559bba29c762cd02826061a
unknown <mx>
2018-04-16 20:43:02 +0800

    增加开发服务

--- app/lib/setupDevServer.js
+++ app/lib/setupDevServer.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+const webpack = reuqire('webpack');
+const { devMiddleware, hotMiddleware } = require('koa-webpack-middleware');
+const clientConfig = require('../../build/webpack.client.config');
+const serverConfig = require('../../build/webpack.server.config');
+
+const readFile = (fs, file) => {
+    try {
+      return fs.readFileSync(path.join(clientConfig.output.path, file), 'utf-8')
+    } catch (e) {}
+};
+
 module.exports = (app, templatePath, ch) => {
     let ready;
     let bundle;
@@ -15,5 +27,29 @@ module.exports = (app, templatePath, ch) => {
         }
     };
 
-    
+    template = fs.readFileSync(templatePath, 'utf-8');
+    fs.watch(templatePath, () => {
+        template = fs.readFileSync(templatePath, 'utf-8')
+        console.log('index.html template updated.')
+        update()
+    });
+
+
+    const clientCompiler = webpack(clientConfig);
+
+    app.use(devMiddleware(clientCompiler, { publicPath: clientConfig.output.publicPath, noInfo: true }));
+
+    clientCompiler.plugin('done', stats => {
+        stats = stats.toJson();
+        stats.errors.forEach(err => console.error(err));
+        stats.warnings.forEach(err => console.warn(err));
+
+        if(!stats.errors.length){
+            clientManifest = JSON.parse(readFile(
+                devMiddleware.fileSystem,
+                'vue-ssr-client-manifest.json'
+                ))
+        }
+        update()
+    });
 };
